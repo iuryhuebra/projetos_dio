eba98a149ac2e84752e4be753456ae8a164f3838
iuryhuebra <iuryhuebra702@gmail.com>
2024-04-07 14:47:00 -0300

    Desafio DotNET 3 #1

---
+++ dotnet/poo-desafio/DesafioPOO/Program.cs
@@ -0,0 +1,14 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Iphone:");
+Iphone celular1 = new Iphone(numero: "425125", modelo: "X", imei: "111111", memoria: 128);
+celular1.Ligar();
+celular1.ReceberLigacao();
+celular1.InstalarAplicativo(nomeApp: "WhatsApp");
+
+Console.WriteLine("\nNokia:");
+Nokia celular2 = new Nokia("214512", "G60", "111111", 32);
+celular2.Ligar();
+celular2.ReceberLigacao();
+celular2.InstalarAplicativo("Instagram");
+
